50c840d56a75074853f37274e2c6384351c3f315
Ramon Nascimento Vanderlei <ramonboladao98@gmail.com>
2025-01-15 15:06:35 -0300

    passagem de codigo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+      path: '',
+      component: LoginComponent,
+  },
+  {
+      path: 'user-infos',
+      component: UserInfosComponent,
+  }
+];
