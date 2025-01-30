a3f3b338cb74dcadc1b8c8eec3aa7db20598bdba
Ramon Nascimento Vanderlei <ramonboladao98@gmail.com>
2025-01-30 15:38:34 -0300

    feat:loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadingService } from './services/loading.service';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'projeto-users-front';
+
+  readonly _loadingService = inject(LoadingService);
+
+  getLoading(){
+    return this._loadingService.loading$;
+  }
 }
---
+++ src/app/interceptors/loading.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { LoadingService } from '../services/loading.service';
+import { inject } from '@angular/core';
+import { finalize } from 'rxjs';
+
+export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const loadingService = inject(LoadingService);
+
+  loadingService.showLoading();
+
+  return next(req).pipe(
+    finalize(()=> {
+      loadingService.hideLoading();
+    })
+  )
+
+  return next(req);
+};
