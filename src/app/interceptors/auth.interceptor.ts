71b06777b775a3a9152940aa961872ad28244ffe
Ramon Nascimento Vanderlei <ramonboladao98@gmail.com>
2025-01-30 14:54:14 -0300

    feat:error interceptors

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,4 +1,5 @@
 import { HttpBackend, HttpContext, HttpContextToken, HttpInterceptorFn } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 export const AUTH_TOKEN_ENABLED = new HttpContextToken<boolean>(() => true);
 
@@ -7,15 +8,26 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   console.log('req', req);
   console.log('context', req.context.get(AUTH_TOKEN_ENABLED));
 
-  let newReq = req;
+  // let newReq = req;
 
   const APPLY_AUTH_TOKEN = req.context.get(AUTH_TOKEN_ENABLED);
 
-  if(APPLY_AUTH_TOKEN){
-    newReq = req.clone({
-      headers: req.headers.set('authorization', 'Bearer ' + localStorage.getItem('token')),
-    })
+  const TOKEN = APPLY_AUTH_TOKEN ? localStorage.getItem('token') : null;
+
+  if(APPLY_AUTH_TOKEN && !TOKEN){
+    return throwError(() => new Error('Token não encontrado.'));
   }
+
+  const newReq = TOKEN ? req.clone({
+    headers: req.headers.set('authorization', 'Bearer ' + localStorage.getItem('token')),
+  }) :req;
+
+  // const APPLY_AUTH_TOKEN = req.context.get(AUTH_TOKEN_ENABLED);
+  // if(APPLY_AUTH_TOKEN){
+  //   newReq = req.clone({
+  //     headers: req.headers.set('authorization', 'Bearer ' + localStorage.getItem('token')),
+  //   })
+  // }
 
 
   // const newReq = req.clone({
